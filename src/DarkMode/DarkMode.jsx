c468cfc60c74c09f45a9b6d074bc5b07fcd07716
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-12 19:55:57 +0530

    dark mood togle button implimented and styled successfull

--- src/DarkMode/DarkMode.jsx
+++ src/DarkMode/DarkMode.jsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState} from 'react';
 import './DarkStyle.css'
 
 function DarkMode(){
-    const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(true);
     
     useEffect(() => {
         document.body.className = darkMode ? 'dark-mode' : 'light-mode';
-      }, [darkMode]);
+    }, [darkMode]);
 
     return(
+    <div className="theme-toggle">
+      <div className="switch">
         <input  type="checkbox"
                 onChange={() => setDarkMode(!darkMode)}
                 checked = {darkMode}
                 id = 'check'
                 className = 'toggle' />
+        <label htmlFor="check" className="slider"/>
+      </div>
+    </div>
     );
 }
 export default DarkMode
