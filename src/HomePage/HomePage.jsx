80f5e1754b3f0ab5b4678ae7b059970b27b792e3
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-14 10:32:50 +0530

    typing animation done

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
 import './HOmeStyle.css'
 import NavigationBar from '../Navigation_Bar/NavigationBar';
+import TypingAnimation from './Typing_Automation/TypingAnimation';
 
 function HomePage(){
     return(
-        <NavigationBar/>
+        <>
+            <NavigationBar/>
+            <div className='center-items'>
+                <div className='text-container'>
+                    <h1>Hi there...! <br/>
+                    I'm Harsha Buddhika
+                    </h1>
+                </div>
+                <TypingAnimation/>
+            </div>
+        </>
     );
 }
 export default HomePage
---
+++ src/HomePage/Typing_Automation/TypingAnimation.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import './TypingAnimation.css';
+
+function TypingAnimation(){
+  const phrases = [
+    "Software Engineer",
+    "Computer Science Undergraduate",
+    "UI/UX Designer",
+    "Web Developer"
+  ];
+  
+  const [displayText, setDisplayText] = useState("");
+  const [phraseIndex, setPhraseIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
+  
+  useEffect(() => {
+    const currentPhrase = phrases[phraseIndex];
+    
+    if (isTyping) {
+      // Typing effect
+      if (displayText.length < currentPhrase.length) {
+        const timer = setTimeout(() => {
+          setDisplayText(currentPhrase.substring(0, displayText.length + 1));
+        }, 100);
+        return () => clearTimeout(timer);
+      }
+      else {
+        // Pause before erasing
+        const timer = setTimeout(() => {
+          setIsTyping(false);
+        }, 1500);
+        return () => clearTimeout(timer);
+      }
+    }
+    else {
+      // Erasing effect
+      if (displayText.length > 0) {
+        const timer = setTimeout(() => {
+          setDisplayText(displayText.substring(0, displayText.length - 1));
+        }, 50);
+        return () => clearTimeout(timer);
+      } else {
+        // Move to next phrase
+        const nextIndex = (phraseIndex + 1) % phrases.length;
+        setPhraseIndex(nextIndex);
+        setIsTyping(true);
+      }
+    }
+  }, [displayText, phraseIndex, isTyping, phrases]);
+  return (
+        <div className="text-container">
+          <span className="dynamic-text">
+            {displayText}
+            <span className="cursor"></span>
+          </span>
+        </div>
+  );
+};
+
+export default TypingAnimation;
