7c29d81d026b5d392f1348a2ad78f07d435f5243
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-25 13:27:16 +0530

    page 2 basic styling done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,13 +12,6 @@ function HomePage(){
                         </h1>
                     </div>
                     <TypingAnimation/>
-                    {/* <p className='paragraph'>
-                        I'm a Computer Science undergraduate at Eastern University,
-                        Trincomalee Campus. I'm a fast learner who grows through
-                        mistakes and constantly works to improve my skills.
-                        Passionate about tech and always ready to take on
-                        new challenges.
-                    </p> */}
                     <a href="">
                         <button className='cv-button'>Download CV</button>
                     </a>
--- src/pages/page2/AboutMe.jsx
+++ src/pages/page2/AboutMe.jsx
@@ -1,8 +1,20 @@
+import './AboutStyling.css'
+import profilePic from './assets/image.jpg';
+
 function AboutMe(){
     return(
-        <>
-            <h1>hello</h1>
-        </>
+        <div className="about-container">
+                <img src={profilePic} alt="Profile Pic" className='profilePic'/>
+            <div className='text-container'>
+                <h1 className='tital-about'>About me</h1>
+                <p className='paragraph'>
+                            I'm a Computer Science undergraduate at Eastern University,Trincomalee Campus.<br/>
+                            I'm a fast learner who grows through mistakes and constantly works to improve my skills. <br/>
+                            Passionate about tech and always ready to take on
+                            new challenges.
+                </p>
+            </div>
+        </div>
     )
 }
 export default AboutMe;
