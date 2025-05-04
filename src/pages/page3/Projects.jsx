27747379fd2dfc79cab92641d4e68821c9b8bdcc
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-05-04 22:14:12 +0530

    project template done

--- src/pages/page3/Projects.jsx
+++ src/pages/page3/Projects.jsx
@@ -1,20 +1,42 @@
 import Template from '../../components/Project_block/template';
 import './Projects.css';
-import project1Image from '../page3/600x300.png'; // Import the image directly
+import project1Image from '../page3/images/600x300.png';
 
 function Projects() {
   const projects = [
     {
-      name: "Project 1",
+      name: "Portfolio - React",
       image: project1Image, 
-      description: "This is the description for Project 1.hasduiopfheuha fush ehfiuhe afhiush difhaweh fhaisdhfihwea ifhishafeuihiahfihd",
+      description: "A responsive and modern portfolio website built using React. It highlights my skills, showcases selected projects, and includes smooth navigation and a clean UI to create a professional online presence.",
       technologies: ["React", "CSS", "JavaScript"], 
       github: "https://github.com/Harsha-Buddhika-Official/Portfolio-React",
+    },
+    {
+      name: "ToDoApp - React",
+      image: project1Image, 
+      description: "A simple and interactive to-do list application built with React. It allows users to add, delete, and mark tasks as completed, with real-time updates using React state management and a clean user interface.",
+      technologies: ["React", "CSS", "JavaScript"], 
+      github: "https://github.com/Harsha-Buddhika-Official/ToDoApp-React",
+    },
+    {
+      name: "Portfolio",
+      image: project1Image, 
+      description: "A responsive and visually appealing portfolio website built using HTML, CSS, and JavaScript. It features smooth scrolling, interactive elements, and showcases my skills, projects, and contact information.",
+      technologies: ["Html", "CSS", "JavaScript"], 
+      github: "https://github.com/Harsha-Buddhika-Official/Portfolio",
+    },
+    {
+      name: "To-Do-List",
+      image: project1Image, 
+      description: " A simple and functional task manager built with HTML, CSS, and JavaScript. It lets users add, delete, and mark tasks as completed with dynamic DOM manipulation and a clean user interface.",
+      technologies: ["Html", "CSS", "JavaScript"], 
+      github: "https://github.com/Harsha-Buddhika-Official/To-Do-List",
     }
+    
   ];
 
   return (
-    <div>
+    <div className="projects-container">
       {projects.map((project, index) => (
         <Template
           key={index}
