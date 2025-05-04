03e217a9a837e623f1e4ec1e8b2cdc08dc39b05e
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-05-04 22:21:30 +0530

    page 3 also done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import NavigationBar from './components/Navigation_Bar/NavigationBar';
 import AboutMe from './pages/page2/AboutMe';
 import Projects from './pages/page3/Projects';
 import './App.css'
-import Template from './components/Project_block/template';
 
 function App() {
   return (
     <>
-      {/* <div className='Navigator'>
+      <div className='Navigator'>
         <NavigationBar/>
       </div>
       <HomePage/>
-      <AboutMe/> */}
+      <AboutMe/>
       <Projects/>
-      {/* <Template/> */}
     </>
   );
 }
